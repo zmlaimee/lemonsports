cb323b94c8c34c58253d61d9f74ab4ee8abc61ee
zmlaimee <2207085506@qq.com>
2019-01-15 18:31:15 +0800

    就好几百

---
+++ product_parent/product_service_8002/src/main/java/cn/lemonsports/mapper/ProductTypeMapper.java
@@ -0,0 +1,16 @@
+package cn.lemonsports.mapper;
+
+import cn.lemonsports.domain.ProductType;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 商品目录 Mapper 接口
+ * </p>
+ *
+ * @author zmltest
+ * @since 2019-01-15
+ */
+public interface ProductTypeMapper extends BaseMapper<ProductType> {
+
+}
