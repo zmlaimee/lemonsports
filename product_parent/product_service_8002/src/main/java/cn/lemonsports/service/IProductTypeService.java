223722f9cb2cef4e6b43fda354a9ed2022aba040
zmlaimee <2207085506@qq.com>
2019-01-15 18:32:20 +0800

    就好几百

---
+++ product_parent/product_service_8002/src/main/java/cn/lemonsports/service/IProductTypeService.java
@@ -0,0 +1,16 @@
+package cn.lemonsports.service;
+
+import cn.lemonsports.domain.ProductType;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ * 商品目录 服务类
+ * </p>
+ *
+ * @author zmltest
+ * @since 2019-01-15
+ */
+public interface IProductTypeService extends IService<ProductType> {
+
+}
