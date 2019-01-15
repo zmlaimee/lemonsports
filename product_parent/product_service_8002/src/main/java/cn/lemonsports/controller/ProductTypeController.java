44ce8ea653a33a1de07cf2b5902a542119b78e1f
zmlaimee <2207085506@qq.com>
2019-01-15 18:30:55 +0800

    就好几百

---
+++ product_parent/product_service_8002/src/main/java/cn/lemonsports/controller/ProductTypeController.java
@@ -0,0 +1,88 @@
+package cn.lemonsports.controller;
+
+import cn.lemonsports.service.IProductTypeService;
+import cn.lemonsports.domain.ProductType;
+import cn.lemonsports.query.ProductTypeQuery;
+import cn.lemonsports.util.AjaxResult;
+import cn.lemonsports.util.PageList;
+import com.baomidou.mybatisplus.plugins.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/productType")
+public class ProductTypeController {
+    @Autowired
+    public IProductTypeService productTypeService;
+
+    /**
+    * 保存和修改公用的
+    * @param productType  传递的实体
+    * @return Ajaxresult转换结果
+    */
+    @RequestMapping(value="/save",method= RequestMethod.POST)
+    public AjaxResult save(@RequestBody ProductType productType){
+        try {
+            if(productType.getId()!=null){
+                productTypeService.updateById(productType);
+            }else{
+                productTypeService.insert(productType);
+            }
+            return AjaxResult.getAjaxResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return AjaxResult.getAjaxResult().setMessage("保存对象失败！"+e.getMessage());
+        }
+    }
+
+    /**
+    * 删除对象信息
+    * @param id
+    * @return
+    */
+    @RequestMapping(value="/{id}",method=RequestMethod.DELETE)
+    public AjaxResult delete(@PathVariable("id") Long id){
+        try {
+            productTypeService.deleteById(id);
+            return AjaxResult.getAjaxResult();
+        } catch (Exception e) {
+        e.printStackTrace();
+            return AjaxResult.getAjaxResult().setMessage("删除对象失败！"+e.getMessage());
+        }
+    }
+
+    //获取用户
+    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
+    public ProductType get(@PathVariable("id")Long id)
+    {
+        return productTypeService.selectById(id);
+    }
+
+
+    /**
+    * 查看所有的员工信息
+    * @return
+    */
+    @RequestMapping(value = "/list",method = RequestMethod.GET)
+    public List<ProductType> list(){
+
+        return productTypeService.selectList(null);
+    }
+
+
+    /**
+    * 分页查询数据
+    *
+    * @param query 查询对象
+    * @return PageList 分页对象
+    */
+    @RequestMapping(value = "/json",method = RequestMethod.POST)
+    public PageList<ProductType> json(@RequestBody ProductTypeQuery query)
+    {
+        Page<ProductType> page = new Page<ProductType>(query.getPage(),query.getRows());
+            page = productTypeService.selectPage(page);
+            return new PageList<ProductType>(page.getTotal(),page.getRecords());
+    }
+}
