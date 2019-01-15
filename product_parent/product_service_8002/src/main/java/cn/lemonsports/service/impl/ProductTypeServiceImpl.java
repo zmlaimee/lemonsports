0fe5e2052d0dc54a656689d4bf71b179a6076fe3
zmlaimee <2207085506@qq.com>
2019-01-15 18:32:35 +0800

    就好几百

---
+++ product_parent/product_service_8002/src/main/java/cn/lemonsports/service/impl/ProductTypeServiceImpl.java
@@ -0,0 +1,20 @@
+package cn.lemonsports.service.impl;
+
+import cn.lemonsports.domain.ProductType;
+import cn.lemonsports.mapper.ProductTypeMapper;
+import cn.lemonsports.service.IProductTypeService;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 商品目录 服务实现类
+ * </p>
+ *
+ * @author zmltest
+ * @since 2019-01-15
+ */
+@Service
+public class ProductTypeServiceImpl extends ServiceImpl<ProductTypeMapper, ProductType> implements IProductTypeService {
+
+}
